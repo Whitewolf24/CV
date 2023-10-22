db0b6a929f5ca40702771f32475c8b7a264cc97b
Whitewolf24 <geo_marinos@yahoo.com>
2023-10-22 08:13:39 +0300

    2.04 "Mobile improvements"

--- src/components/portfolio_gr.tsx
+++ src/components/portfolio_gr.tsx
@@ -1969,10 +1969,10 @@ if (navigator.userAgent.includes("Chrome") && navigator.userAgent.includes("Mobi
 if (navigator.userAgent.includes("Firefox") && navigator.userAgent.includes("Mobi")) {
     onload = () => {
         const footer = document?.querySelector("footer") as HTMLDivElement;
-        /*document.body.style.cssText += "overflow: hidden scroll !important";
-      footer.style.cssText += "bottom: 0rem !important";*/
+        /*document.body.style.cssText += "overflow: hidden scroll !important";*/
+        footer.style.cssText += "bottom: auto !important";
         footer.style.cssText += "position: relative !important";
-        footer.style.cssText += "top: 73rem !important";
+        footer.style.cssText += "top: 43rem !important";
     }
 }
 
