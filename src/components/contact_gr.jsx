4adea1e6681977751abed7c38bb260f051e9e155
Whitewolf24 <geo_marinos@yahoo.com>
2023-10-23 05:22:07 +0300

    2.06 "Rearranged Portfolio/ Added Email/ Bug Fixes"

--- src/components/contact_gr.jsx
+++ src/components/contact_gr.jsx
@@ -73,7 +73,7 @@ const Contact_gr = () => {
                 }
             }
         }}>
-            <p style={{ fontSize: " clamp(.7rem, 2.5vw, .8rem)", marginBottom: "2rem", marginInline: "auto", lineHeight: "1.4rem", whiteSpace: "pre-line", width: "58ch" }}>Για να επικοινωνήσετε μαζί μου συμπληρώστε την παρακάτω φόρμα, με το email σας και το μήνυμα σας, ή γράψτε μου στο&nbsp;
+            <p className="contact_desc" style={{ fontSize: " clamp(.7rem, 2.5vw, .8rem)", marginBottom: "2rem", marginInline: "auto", lineHeight: "1.4rem", whiteSpace: "pre-line", width: "58ch" }}>Για να επικοινωνήσετε μαζί μου συμπληρώστε την παρακάτω φόρμα, με το email σας και το μήνυμα σας, ή γράψτε μου στο&nbsp;
                 <a style={{ color: "white" }} href="mailto:contactme@marinoscv.website" target="_blank">contactme@marinoscv.website</a> </p>
             <form className="contact_form" ref={form} onSubmit={handleSubmit(submit_form)} /*method="POST"  data-sitekey="vlmZE8H22O2e_lQFj"*/>
                 <div>
